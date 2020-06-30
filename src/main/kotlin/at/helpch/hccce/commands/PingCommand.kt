1e86c00d91d741df1af82e374db7c457efbd38ce
Jaimss <james@harrell365.com>
2020-06-30 18:36:43 -0400

    forgot my start/end comments

--- src/main/kotlin/at/helpch/hccce/commands/PingCommand.kt
+++ src/main/kotlin/at/helpch/hccce/commands/PingCommand.kt
@@ -6,9 +6,10 @@ import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 
 class PingCommand(plugin: HCCCE) : CommandExecutor {
-
     init {
+        // Jaims Start
         plugin.getCommand("ping")?.setExecutor(this) // TODO: null check here
+        // Jaims end
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
