7a6e8f384cdc12b43cf0bfeb8eeafb559cf3b0e8
Jaimss <james@harrell365.com>
2020-06-30 18:34:25 -0400

    get the ping command shell
    i counted my characters not including comments and not including imports
    if i'm supposed to include them just don't merge this i guess

--- src/main/kotlin/at/helpch/hccce/commands/PingCommand.kt
+++ src/main/kotlin/at/helpch/hccce/commands/PingCommand.kt
@@ -1,3 +1,18 @@
 package at.helpch.hccce.commands
 
-class PingCommand // TODO: Please someone make this work! I would have but I ran out of chars lol
+import at.helpch.hccce.HCCCE
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
+class PingCommand(plugin: HCCCE) : CommandExecutor {
+
+    init {
+        plugin.getCommand("ping")?.setExecutor(this) // TODO: null check here
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        TODO("Add command functionality lol")
+    }
+
+}
