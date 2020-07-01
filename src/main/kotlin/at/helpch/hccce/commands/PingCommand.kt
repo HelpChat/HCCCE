031c2c21ef22a6b07b461e9fa8c1889de9aea1b0
BomBardyGamer <callum.seabrook@prevarinite.com>
2020-07-01 15:01:09 +0100

    Added licensing, made PingCommand work, made RespawnEvent a Kotlin file.

--- src/main/kotlin/at/helpch/hccce/commands/PingCommand.kt
+++ src/main/kotlin/at/helpch/hccce/commands/PingCommand.kt
@@ -5,7 +5,12 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 
+/**
+ * @author Callum Seabrook
+ * @author Jaimss
+ */
 class PingCommand(plugin: HCCCE) : CommandExecutor {
+
     init {
         // Jaims Start
         plugin.getCommand("ping")?.setExecutor(this) // TODO: null check here
@@ -13,7 +18,9 @@ class PingCommand(plugin: HCCCE) : CommandExecutor {
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        TODO("Add command functionality lol")
+        // BomBardyGamer - start
+        sender.sendMessage("Pong!")
+        return true
+        // BomBardyGamer - end
     }
-
 }
